8f37d705082119b919875ec774395763cfea9ce1
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-09-14 01:27:56 +0200

    add score partitions code

--- MusicMetaLinker/audio_partitions.py
+++ MusicMetaLinker/audio_partitions.py
@@ -24,14 +24,14 @@ Notes for the alignment:
 from pathlib import Path
 
 import pandas as pd
-from partitions_map import AUDIO_PARTITIONS
+from partitions_map import AUDIO_PARTITIONS, SCORE_PARTITIONS
 from clean_partitions import clean_billboard
 from linking import linking
 from preprocessor import JAMSProcessor
 from utils import log_downloaded_data
 
 
-def retrieve_links(partitions_path: Path,
+def retrieve_links_audio(partitions_path: Path,
                    save: bool = True,
                    download: bool = False):
     """
@@ -136,5 +136,98 @@ def retrieve_links(partitions_path: Path,
         log_downloaded_data(df, partition / "choco" / "linking.csv")
 
 
+def retrieve_links_score(partitions_path: Path,
+                         save: bool = True,
+                         download: bool = False) -> None:
+    """
+    Retrieves the links for the score partitions.
+    Parameters
+    ----------
+    partitions_path : Path
+        Path to the partitions.
+    save : bool
+        Whether to save the retrieved information in a new JAMS file or not.
+    download : bool
+        Whether to download the audio files or not.
+    Returns
+    -------
+    None
+    """
+    # iterate over the partitions
+    for partition in partitions_path.iterdir():
+        if partition.name not in SCORE_PARTITIONS:
+            continue
+        print(f"Processing partition {partition.name}")
+        # initialize data to be stored in a dataframe
+        df_list = []
+
+        # partition/choco/jams_converted if exists else partition/choco/jams
+        jams_converted = partition / "choco" / "jams-converted"
+        if jams_converted.exists():
+            jams_path = jams_converted
+        else:
+            jams_path = partition / "choco" / "jams"
+        if partition.name == "ireal-pro":
+            jams_path = partition / "choco" / "playlist" / "jams-converted"
+        # iterate over the JAMS files
+        for jams_file in jams_path.glob("*.jams"):
+            print(f"Processing JAMS file {jams_file.name}")
+            # process the JAMS file
+            jams_process = JAMSProcessor(jams_file)
+            # retrieve the links
+            linker = linking.Align(
+                artist=jams_process.artist_name,
+                album=jams_process.album_name,
+                track=jams_process.track_name,
+                track_number=jams_process.track_number,
+                duration=jams_process.duration,
+                strict=False,
+            )
+            track_name = linker.get_track()
+            artist_name = linker.get_artist()
+            album_name = linker.get_album()
+            track_number = linker.get_track_number()
+            duration = linker.get_duration()
+            release_year = linker.get_release_date()
+            # get the identifiers
+            original_identifiers = jams_process.identifiers
+            links = {'musicbrainz': linker.get_mbid(),
+                     'isrc': linker.get_isrc(),
+                     'deezer': linker.get_deezer_id(),
+                     'deezer_url': linker.get_deezer_link(),
+                     'youtube_url': linker.get_youtube_link(),
+                     }
+
+            # add retrieved information to the JAMS file
+            jams_process.track_name = track_name
+            jams_process.artist_name = artist_name
+            jams_process.album_name = album_name
+            jams_process.track_number = track_number
+            jams_process.duration = duration
+            jams_process.release_year = release_year
+            jams_process.identifiers = {**original_identifiers, **links}
+
+            # store information in a dataframe
+            df_list.append({'jams_file': jams_file.name,
+                            'track_name': track_name,
+                            'artist_name': artist_name,
+                            'album_name': album_name,
+                            'track_number': track_number,
+                            'duration': duration,
+                            'release_year': release_year,
+                            'musicbrainz': linker.get_mbid(),
+                            'isrc': linker.get_isrc(),
+                            'deezer_id': linker.get_deezer_id(),
+                            'deezer_url': linker.get_deezer_link(),
+                            'youtube_url': linker.get_youtube_link(),
+                            'acousticbrainz': linker.get_acousticbrainz_link(),
+                            })
+
+            if save:
+                save_path = jams_path.parent / "jams_aligned"
+                jams_process.write_jams(save_path)
+
+
 if __name__ == "__main__":
-    retrieve_links(Path("../partitions"))
+    #retrieve_links_audio(Path("../partitions"))
+    retrieve_links_score(Path("../partitions"))
